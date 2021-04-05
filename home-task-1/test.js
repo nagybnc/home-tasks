d3ec5a88606d8635dcea628a77d639f3bb7633e3
Gergely Bence Nagy <gergely_bence_nagy@epam.com>
2021-04-05 15:25:11 +0200

    Add Insert function tests

--- home-task-1/test.js
+++ home-task-1/test.js
@@ -5,4 +5,16 @@ describe('Node', () => {
   test('Node is a constructor', () => {
     expect(typeof Node.prototype.constructor).toEqual('function');
   });
+
+  test('Node can insert correctly', () => {
+    const node = new Node(15);
+    node.insert(10);
+    node.insert(25);
+    node.insert(27);
+  
+    expect(node.left.data).toEqual(10);
+    expect(node.right.data).toEqual(25);
+    expect(node.right.right.data).toEqual(27);
+  });
+
 })
