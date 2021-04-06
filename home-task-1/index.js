ebfee2f57c45b0a495cf1982f3d376b4e0fd77d6
Gergely Bence Nagy <gergely_bence_nagy@epam.com>
2021-04-06 11:20:56 +0200

    Add BF and DF traverse

--- home-task-1/index.js
+++ home-task-1/index.js
@@ -1,21 +1,37 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.left = null;
-    this.right = null;
+class Tree {
+  constructor() {
+    this.root = null;
   }
 
-  insert(data) {
-    if (data < this.data && this.left) {
-      this.left.insert(data);
-    } else if (data < this.data) {
-      this.left = new Node(data);
-    } else if (data > this.data && this.right) {
-      this.right.insert(data);
-    } else if (data > this.data) {
-      this.right = new Node(data);
+  traverseBF(fn) {
+    const arr = [this.root];
+    while (arr.length) {
+      const node = arr.shift();
+
+      arr.push(...node.children);
+      fn(node);
+    }
+  }
+
+  traverseDF(fn) {
+    const arr = [this.root];
+    while (arr.length) {
+      const node = arr.shift();
+      arr.unshift(...node.children);
+      fn(node);
     }
   }
 }
 
-module.exports = { Node };
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
+
+  add(data) {
+    this.children.push(new Node(data));
+  }
+}
+
+module.exports = {Node, Tree}
--- home-task-1/test.js
+++ home-task-1/test.js
@@ -1,20 +1,77 @@
-const T = require('./index');
+const T = require("./index");
 const Node = T.Node;
+const Tree = T.Tree;
 
-describe('Node', () => {
-  test('Node is a constructor', () => {
-    expect(typeof Node.prototype.constructor).toEqual('function');
+describe("Node", () => {
+  test("Node is a constructor", () => {
+    expect(typeof Node.prototype.constructor).toEqual("function");
   });
 
-  test('Node can insert correctly', () => {
-    const node = new Node(15);
-    node.insert(10);
-    node.insert(25);
-    node.insert(27);
-  
-    expect(node.left.data).toEqual(10);
-    expect(node.right.data).toEqual(25);
-    expect(node.right.right.data).toEqual(27);
+  test("Node has a data and children properties", () => {
+    const n = new Node("a");
+    expect(n.data).toEqual("a");
+    expect(n.children.length).toEqual(0);
   });
 
-})
+  test("Node can add children", () => {
+    const n = new Node("a");
+    n.add("b");
+    expect(n.children.length).toEqual(1);
+    expect(n.children[0].children).toEqual([]);
+  });
+});
+
+describe("Tree", () => {
+  test("starts empty", () => {
+    const t = new Tree();
+    expect(t.root).toEqual(null);
+  });
+
+  test("Can traverse bf", () => {
+    const letters = [];
+    const t = new Tree();
+    t.root = new Node("15");
+    t.root.add("10");
+    t.root.add("25");
+    t.root.children[0].add("7");
+    t.root.children[0].add("13");
+    t.root.children[1].add("22");
+    t.root.children[1].add("27");
+    /*
+    "       15       ",
+    "      /  \\     ",
+    "     10   25    ",
+    "    / \\   / \\ ",
+    "   7  13 22  27 ",
+    */
+    t.traverseBF((node) => {
+      letters.push(node.data);
+    });
+
+    expect(letters).toEqual(["15", "10", "25", "7", "13", "22", "27"]);
+  });
+
+  test("Can traverse DF", () => {
+    const letters = [];
+    const t = new Tree();
+    t.root = new Node("15");
+    t.root.add("10");
+    t.root.add("25");
+    t.root.children[0].add("7");
+    t.root.children[0].add("13");
+    t.root.children[1].add("22");
+    t.root.children[1].add("27");
+    /*
+    "       15       ",
+    "      /  \\     ",
+    "     10   25    ",
+    "    / \\   / \\ ",
+    "   7  13 22  27 ",
+    */
+    t.traverseDF((node) => {
+      letters.push(node.data);
+    });
+
+    expect(letters).toEqual(["15", "10", "7", "13", "25", "22", "27"]);
+  });
+});
